e69adf4f62e376edba6e88a771abeaaca1698f75
Berg <david.berg@pliktverket.se>
2021-04-07 09:52:09 +0200

    Added som code and configuration

---
+++ TheClient/src/TheClient.java
@@ -0,0 +1,7 @@
+package se.woop;
+
+public class TheClient {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ TheLibrary/src/TheLibrary.java
@@ -0,0 +1,7 @@
+package se.woop;
+
+public class TheLibrary {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
