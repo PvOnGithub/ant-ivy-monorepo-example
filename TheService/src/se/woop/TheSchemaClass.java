9bd4dc5335f56fb8716b4f028b93588f91f2094e
Berg <david.berg@pliktverket.se>
2021-04-22 09:46:59 +0200

    Only rebuild on change seems to work for dependencies as well. More testing might be needed-

--- TheService/src/se/woop/TheSchemaClass.java
+++ TheService/src/se/woop/TheSchemaClass.java
@@ -3,7 +3,7 @@ package se.woop;
 public class TheSchemaClass {
     
     public static void doSomething() {
-        System.out.println("I am doing something!");
+        System.out.println("I am doing something!!");
     }
 
 }
