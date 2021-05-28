d740bce2b2aac11ccfc1a720ef399e3d99d5dc97
Berg <david.berg@pliktverket.se>
2021-05-28 09:30:35 +0200

    Testing hesting

--- TheService/src/se/woop/TheService.java
+++ TheService/src/se/woop/TheService.java
@@ -2,6 +2,6 @@ package se.woop;
 
 public class TheService {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!!");
     }
 }
