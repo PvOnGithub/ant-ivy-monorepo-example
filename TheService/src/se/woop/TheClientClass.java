969224651212301fecad7abc45520602c9146a52
Stefan Johannesdal <stefan.johannesdal@gmail.com>
2021-06-04 10:42:26 +0200

    Städat lite ;-)

--- TheService/src/se/woop/TheClientClass.java
+++ TheService/src/se/woop/TheClientClass.java
@@ -1,6 +1,10 @@
 package se.woop;
 
 public class TheClientClass {
+
+    private TheClientClass() {
+        throw new IllegalStateException("TheClientClass");
+    }
     
     public static boolean isThisTrue(boolean b) {
         boolean a = b;
