420785748808f70436aae89882b3242674667595
Stefan Johannesdal <stefan.johannesdal@gmail.com>
2021-06-04 10:45:55 +0200

    Mer städning

--- TheService/src/se/woop/TheSchemaClass.java
+++ TheService/src/se/woop/TheSchemaClass.java
@@ -2,6 +2,10 @@ package se.woop;
 
 public class TheSchemaClass {
     
+    private TheSchemaClass() {
+        throw new IllegalStateException("TheClientClass");
+    }
+    
     public static void doSomething() {
         System.out.println("I am doing something!!");
     }
